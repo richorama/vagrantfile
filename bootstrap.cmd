8134c422d62ab48a2903663548c813183326d92c
Richard Astbury <richard.astbury@gmail.com>
2014-08-28 14:28:02 +0100

    initial commit

---
+++ bootstrap.cmd
@@ -0,0 +1,3 @@
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+netsh firewall set opmode disable
+netsh firewall set opmode disable disable all
